a43e10ee4627f5e3967be1a7456004311288fd9a
Shiva <tak2siva@gmail.com>
2018-03-16 12:07:32 +0530

    Update liste.rb

--- liste.rb
+++ liste.rb
@@ -1,14 +1,28 @@
 java_path = "./SPICinemas-CI-Config/artifacts/"
+js_path = "./SPICinemas-CI-Config/static-assets/"
 
 while true
 	if File.exist?(java_path + "restart.txt")
-		puts "Deploying.."
+		puts "Deploying API.."
 
-		`cp #{java_path}/cinemas-api.jar" SPICinemas-API-Docker/artifacts/`
+		`cp #{java_path}/cinemas-api.jar SPICinemas-API-Docker/artifacts/`
 
-		`rm restart.txt`
+		`rm #{java_path}/restart.txt`
 
 		`(cd SPICinemas-API-Docker; docker-compose down -v; docker-compose up -d)`
 	end
+
+	if File.exist?(js_path + "restart.txt")
+		puts "Deploying UI.."
+
+		`cp -R #{js_path}/ SPICinemas-API-Docker/static-assets/`
+
+		`rm #{js_path}/restart.txt`
+
+		# `(cd SPICinemas-API-Docker; docker-compose down -v; docker-compose up -d)`
+	end
+
+	puts "==================="
+
 	sleep(5)
 end
